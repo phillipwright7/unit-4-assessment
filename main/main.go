290d0bc69ac9494c3444d6d47c9a99060f9092a9
phillipwright7 <phillipwright27@icloud.com>
2023-03-06 09:28:55 -0600

    add calculate package

---
+++ main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/phillipwright7/unit-4-assessment/calculate"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println(calculate.Calculate(8, 9))
+}
